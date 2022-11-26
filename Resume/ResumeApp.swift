56bc94ed9f118ec3298782f5fa5064d8e3f4ed8b
“Angela <kelly912718@gmail.com>
2022-11-26 14:37:11 +0800

    Initial Commit

---
+++ Resume/ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by 黃郁雯 on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
